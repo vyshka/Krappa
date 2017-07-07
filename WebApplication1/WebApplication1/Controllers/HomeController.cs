73361b76bd803db0a142c5b3325c6c4ba6b8eacb
vyshka <egorvyshka@gmail.com>
2017-07-07 19:46:43 +0300

    add a part of About company page

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            return RedirectToAction("Index");
+            return View();
         }
     }
 }

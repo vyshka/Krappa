767de8aaa63553ba614865501aa8df1f8095d41e
vyshka <egorvyshka@gmail.com>
2017-07-25 19:05:06 +0300

    update vacancyList, now displaying using web api

---
+++ WebApplication1/WebApplication1/Content/js/web-api.js
@@ -0,0 +1,47 @@
+﻿var urlget = "/api/vacancies/getallVacancies";
+var urldel = "/api/vacancies/DeleteVacancy/";
+var spinner;
+$(document).ready( function(){
+    load();
+})
+function load() {
+    var tbody = document.getElementById("table");
+    var opt = {
+        top: "106%"
+    };
+    spinner = new Spinner(opt).spin(tbody);
+    setTimeout(getData, 1000);
+}
+function getData() {
+    $.getJSON(urlget)
+        .done(function (data) {
+            spinner.stop();
+            $.each(data, function (key, item) {
+                var link = $("<a>");
+                link.on("click", function () {
+                    if (confirm('Вы действительно хотите удалить?')) {
+                        $.ajax({
+                            url: urldel + item.vacancyId,
+                            type: 'DELETE',
+                            success: clearData
+                        });
+                    }
+                    
+                });
+                $("<span />").addClass("glyphicon glyphicon-trash").appendTo(link);
+
+                $("<tr>").append(
+                    $("<td>").text(item.name),
+                    $("<td>").text(item.vacancyUrl).addClass("hidden-xs hidden-sm"),
+                    $("<td>").text(item.City),
+                    $("<td>").append(link)
+                    ).appendTo($("table"));
+                
+            })
+        })
+
+}
+function clearData() {
+    var tbody = $("#table").find("tbody").empty();
+    load();
+}
--- WebApplication1/WebApplication1/Global.asax.cs
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Http;
 using System.Web.Routing;
 using WebApplication1.App_Start;
 using System.Data.Entity;
@@ -15,8 +16,13 @@ namespace WebApplication1
         {
             Database.SetInitializer<MainDbContext>(null);
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+            GlobalConfiguration.Configuration.EnsureInitialized();
+
         }
     }
 }

6ca98919f80fd7509556202629adbbfedd40105f
vyshka <egorvyshka@gmail.com>
2017-08-14 19:24:23 +0300

    remove some refs

--- WebApplication1/WebApplication1/Controllers/SurveyController.cs
+++ WebApplication1/WebApplication1/Controllers/SurveyController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using WebApplication1.Models;
 using System.Web.Http;
-using RefactorThis.GraphDiff;
 using System.Data.Entity;
 using System.Threading;
 

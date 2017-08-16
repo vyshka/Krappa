bb99aa4bb9d6e6b1207d91094e4b70a49c6a52b6
vyshka <egorvyshka@gmail.com>
2017-08-16 19:32:53 +0300

    upload api controllers

---
+++ WebApplication1/WebApplication1/Controllers/Api/ResultController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class ResultController : ApiController
+    {
+        ApplicationContext db = new ApplicationContext();
+
+        public void SaveResult([FromBody] ResultModel model)
+        {
+            var result = db.Results.Create();
+            var user = User.Identity.GetUserId();
+            var survey = db.Surveys.Find(model.surveyId);
+
+            result.Survey = survey;
+            result.User = user;
+
+            foreach(var answerResult in model.AnswersQuestions)
+            {
+                var dbAnswerResult = db.AnswerQuestionResults.Create();
+                var answer = db.Answers.Find(answerResult.answerId);
+                var question = db.Questions.Find(answerResult.questionId);
+                dbAnswerResult.Answer = answer;
+                dbAnswerResult.Question = question;
+                db.AnswerQuestionResults.Add(dbAnswerResult);
+                db.SaveChanges();
+                result.AnswerQuestionResult.Add(dbAnswerResult);
+            }
+            db.Results.Add(result);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/Api/SurveyController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Threading;
+using System;
+
+namespace WebApplication1.Controllers
+{
+    
+    public class SurveyController : ApiController
+    {
+        private ApplicationContext db = new ApplicationContext();
+        
+
+        public bool DeleteSurvey(int id)
+        {
+            var survey = db.Surveys.Find(id);
+            if(survey == null)
+            {
+                return false;
+            }
+            db.Questions.RemoveRange(db.Questions.Where(x => x.Survey.Id == survey.Id));
+            db.Surveys.Remove(survey);
+            db.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<Survey> GetAllSurveys()
+        {
+            Thread.Sleep(1500);
+            var Qlist = db.Surveys.ToList();
+            var Alist = Qlist.SelectMany(p => p.Questions).ToList();
+
+            
+            return Qlist;
+        }
+
+        public Survey GetSurveyById(int id)
+        {
+            var survey = db.Surveys.Find(id);
+
+            return survey;
+        }
+
+        public int CreateSurvey(Survey model)
+        {
+            if(ModelState.IsValid)
+            {
+                var Survey = db.Surveys.Create();
+                Survey.name = model.name;
+                Survey.Questions = model.Questions;
+                db.Surveys.Add(Survey);
+                db.SaveChanges();
+                return Survey.Id;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        [HttpPost]
+        public bool UpdateSurvey(Survey model) //if answer[index].id == null answer.Question = ...
+        {
+
+            var original = db.Surveys
+                                .Include(x => x.Questions)
+                                .Single(c => c.Id == model.Id);
+
+            if (original != null)
+            {
+                db.Entry(original).CurrentValues.SetValues(model);
+                var modelAnswers = new List<Answer>();
+                var originalAnswers = new List<Answer>();
+
+
+                foreach (var originalQuestion in original.Questions.ToList())   //list of questions from db
+                {
+                    foreach (var originalAnswer in originalQuestion.Answers.ToList())
+                    {
+                        originalAnswers.Add(originalAnswer);
+                    }
+
+                }
+
+                foreach (var modelQuestion in model.Questions.ToList())     //list of questions from input model
+                {
+                    foreach (var modelAnswer in modelQuestion.Answers.ToList())
+                    {
+                        modelAnswers.Add(modelAnswer);
+                    }
+                    
+                }
+
+                foreach (var dbQuestion in original.Questions.ToList())     //remove answers, qeustions that doesnot exist
+                {
+
+                    foreach(var dbAnswer in dbQuestion.Answers.ToList())
+                    {
+                        if(!modelAnswers.Any(a => a.Id == dbAnswer.Id))
+                        {
+                            db.Answers.Remove(dbAnswer);
+                        }
+                    }
+
+                    if(!model.Questions.Any(s => s.Id == dbQuestion.Id))
+                    {
+                        db.Questions.Remove(dbQuestion);
+                    }
+                }
+
+
+
+                foreach(var question in model.Questions)        //update questions, answers if they are already exist or add if not
+                {
+
+                    var dbQuestion = original.Questions.SingleOrDefault(q => q.Id == question.Id);
+                    if(dbQuestion != null)
+                    {
+                        db.Entry(dbQuestion).CurrentValues.SetValues(question);
+                    }
+                    else
+                    {
+                        original.Questions.Add(question);
+                    }
+
+                    foreach(var answer in question.Answers)
+                    {
+                        var dbAnswers = originalAnswers.SingleOrDefault(a => a.Id == answer.Id);
+
+                        if (dbAnswers != null)
+                        {
+                            db.Entry(dbAnswers).CurrentValues.SetValues(answer);
+                        }
+                        else
+                        {
+                            answer.Question = question;
+                            db.Answers.Add(answer);
+                        }
+
+                    }
+                }
+
+                original.updateTime = DateTime.Now.ToString();
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        
+
+
+
+    }
+}

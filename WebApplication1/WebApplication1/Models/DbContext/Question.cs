43932647210f52b666163dd438bff9c569e98f72
vyshka <egorvyshka@gmail.com>
2017-08-24 19:06:22 +0300

    update question model

--- WebApplication1/WebApplication1/Models/DbContext/Question.cs
+++ WebApplication1/WebApplication1/Models/DbContext/Question.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace WebApplication1.Models
 {
@@ -12,12 +10,17 @@ namespace WebApplication1.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Text { get; set; }
+
         public virtual ICollection<Option> Options { get; set; }
 
         public virtual Survey Survey { get; set; }
 
         public int Order { get; set; }
+
+        [DefaultValue("options")]
+        public string QuestionType { get; set; }
 
         public Question()
         {

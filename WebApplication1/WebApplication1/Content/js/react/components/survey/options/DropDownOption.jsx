19bf94512d13a0c65afa24cdb24d0df2447f17f8
vyshka <egorvyshka@gmail.com>
2017-08-28 20:03:47 +0300

    upload helpers

---
+++ WebApplication1/WebApplication1/Content/js/react/components/survey/options/DropDownOption.jsx
@@ -0,0 +1,46 @@
+import React from 'React'
+
+
+export class DropDownOption extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedValue: this.props.options[0].Text
+        }
+
+        this.onChange = this.onChange.bind(this)
+    }
+
+    onChange(e) {
+        //Kappa123
+        console.log
+        this.setState({
+            selectedValue: e.target.value
+        })
+        this.props.onChange(e.target.value)
+    }
+
+    render() {
+        var self = this
+        var Options = this.props.options.map(function(element, index) {
+            return(
+                <option 
+                    value = {element.Text} 
+                    key = {index}
+                >
+                    {element.Text}
+                </option>
+            )
+        });
+        return(
+            <select 
+                className = "form-control"
+                onChange = {this.onChange}
+                value = {this.state.selectedValue}
+            >
+                {Options}
+            </select>
+        )
+    }
+}
